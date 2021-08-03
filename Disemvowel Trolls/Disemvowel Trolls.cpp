f693a9f96dbfd9d8f06591e00f48a5c361665549
Eliott <eliott_nu@mail.ru>
2021-08-03 17:44:20 +0900

    Turnkey solution

--- Disemvowel Trolls/Disemvowel Trolls.cpp
+++ Disemvowel Trolls/Disemvowel Trolls.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+string disemvowel(const string& str) {
+    string res;
+    for (auto c : str)
+    {
+        if (!strchr("aeiouAEIOU", c))
+        {
+            res += c;
+        }
+    }
+    return res;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << disemvowel("This website is for losers LOL!") << endl; // Ths wbst s fr lsrs LL!
 }
+
 
 
